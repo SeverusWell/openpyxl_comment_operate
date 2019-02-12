056f19fb0da612be0358cab37259f3ab386f17d6
gitwell <thinkweiwei@msn.com>
2019-02-12 09:40:44 +0800

    split

--- openpyxl_demo.py
+++ openpyxl_demo.py
@@ -5,36 +5,18 @@ from openpyxl import Workbook
 
 source_wb = load_workbook("input.xlsx")
 
-source_sheet = source_wb.get_sheet_by_name("Sheet1")
+source_sheet = source_wb["Sheet1"]
+# source_sheet = source_wb.get_sheet_by_name("Sheet1")
 
 target_wb = Workbook()
 target_sheet = target_wb.active
 
 for row in source_sheet.iter_rows():
     for cell in row:
-        target_sheet[cell.coordinate] = str(cell.comment) if cell.comment else ''
+        print(cell.value)
+        # 内容
+        target_sheet[cell.column + str(cell.row * 2 - 1)] = str(cell.value) if cell.value else ''
+        # 备注
+        target_sheet[cell.column + str(cell.row * 2)] = str(cell.comment) if cell.comment else ''
 
 target_wb.save('output.xlsx')
-
-
-# print(cell.column + '  ' + str(cell.row))
-# print(cell.coordinate, cell.value)
-
-# print(sheet)
-# print(sheet["C"])
-# print(sheet["4"])
-# print(sheet["C4"].value)
-# print(sheet.max_row)
-# print(sheet.max_column)
-
-# for row in source_sheet:
-#     print('')
-#     # target_sheet['A' + str(item.row)] = str(item.comment) if item.comment else ''
-#     #
-#     # # print(item.comment)
-#     # # print('A' + str(item.row))
-#     # # target_sheet['A' + str(item.row)] = item.comment
-#     # # print(i.value, end=", ")
-#     # # print(i.comment, end=" \r\n")
-#
-
