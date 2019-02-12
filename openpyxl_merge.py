c125e00962fd1a21ba227e02b8ede32d0ba44777
gitwell <thinkweiwei@msn.com>
2019-02-12 10:23:36 +0800

    merge

---
+++ openpyxl_merge.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from openpyxl import load_workbook
+from openpyxl import Workbook
+from openpyxl.comments import Comment
+
+source_wb = load_workbook("input.xlsx")
+source_sheet = source_wb.active
+
+target_wb = Workbook()
+target_sheet = target_wb.active
+
+target_current_row = 1
+
+for inx, row in enumerate(source_sheet.iter_rows()):
+    if 0 == inx % 2:  # 内容
+        for cell in row:
+            target_sheet[cell.column + str(target_current_row)] = cell.value
+    else:  # 批注
+        for cell in row:
+            if cell.value:
+                target_sheet[cell.column + str(target_current_row)].comment = Comment(cell.value, 'Python')
+        target_current_row += 1
+
+target_wb.save('output.xlsx')
